ddefd541a5f3a7b50daf893a397875736bae677c
yem297 <yemiigordillo@gmail.com>
2024-06-17 01:26:00 -0300

    Ciclo del Menu

--- menu.cpp
+++ menu.cpp
@@ -6,19 +6,20 @@ using namespace std;
 void menu(){
     int opcion;
 
-    cout << "Menu Principal" << endl;
-    cout << "1 -- Un jugador" << endl;
-    cout << "2 -- Dos jugadores" << endl;
-    cout << "3 -- Estadisticas" << endl;
-    cout << "4 -- Creditos" << endl;
-    cout << "0 -- Salir" << endl;
+    do{
+        cout << "Menu Principal" << endl;
+        cout << "1 -- Un jugador" << endl;
+        cout << "2 -- Dos jugadores" << endl;
+        cout << "3 -- Estadisticas" << endl;
+        cout << "4 -- Creditos" << endl;
+        cout << "0 -- Salir" << endl;
 
     cin >> opcion;
 
     switch(opcion){
-case 0:
-    return;
-    break;
-    }
-
+    case 0:
+        return;
+        break;
+        }
+    }while (opcion!=0);
 }
