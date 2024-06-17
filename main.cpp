6b03fc8c4fbb3a3ded63d50e64505604901ae5fc
yem297 <yemiigordillo@gmail.com>
2024-06-17 01:19:40 -0300

    muestra menu

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include "menu.cpp"
+#include "menu.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(){
+
+    menu();
+
+return 0;
 }
--- menu.cpp
+++ menu.cpp
@@ -2,3 +2,23 @@
 #include "menu.h"
 
 using namespace std;
+
+void menu(){
+    int opcion;
+
+    cout << "Menu Principal" << endl;
+    cout << "1 -- Un jugador" << endl;
+    cout << "2 -- Dos jugadores" << endl;
+    cout << "3 -- Estadisticas" << endl;
+    cout << "4 -- Creditos" << endl;
+    cout << "0 -- Salir" << endl;
+
+    cin >> opcion;
+
+    switch(opcion){
+case 0:
+    return;
+    break;
+    }
+
+}
